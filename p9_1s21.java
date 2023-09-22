ca0c8ea265045263a277bef58b0a4336a783bfb8
BrianRubenGonzalezBlanco <144179421+BrianRubenGonzalezBlanco@users.noreply.github.com>
2023-09-22 12:04:44 -0600

    1s21
    Gonzalez Blanco Brian Ruben 1s21

---
+++ p9_1s21.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class p9_1s21{
+	   public static void main(String[] args){
+		      Scanner teclado = new Scanner(System.in);
+		      String operacion;
+		      System.out.println("que tipo de operacion desea realizar");
+		      System.out.println("escribala: suma, resta, division, multipilicacion");
+		      operacion = teclado.nextLine();
+
+		      if (operacion.equals("Suma")){
+			  System.out.println("Haremos una suma");
+		      } else if (operacion.equals("Resta")){
+			  System.out.println("Haremos una resta");
+		      } else if (operacion.equals("Division")){
+		      System.out.println("Haremos una Division");
+		      } else if (operacion.equals("Multipilicacion")){
+		       System.out.println("Haremos una multipilicacion");
+		      } else {
+    		  System.out.println("No existe la operacion");	
+    		}
+    	}
+    }
+
+	
+
+
+
+
+
+
+
+
+
+			 
+	
